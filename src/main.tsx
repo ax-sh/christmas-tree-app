fcb0719aed62db8683b1b88144502d99f94e5794
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-02-03 05:51:42 +0545

    feat: update ga4

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-HC2JMRH067");
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
