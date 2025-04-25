7acc7c3a1ddd63ee8d6032402f849d7d678a7023
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-04-25 16:56:06 +0545

    chore: update react 19

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
-
-# A three.js and react-fiber project for Christmas 
+# A three.js and react-fiber project for Christmas
 
 ### [WEBSITE](https://ax-sh.github.io/christmas-tree-app/)
 
-## Tech Stack 
+## Tech Stack
+
 - @react-three/drei
 - @react-three/fiber
 - leva
 - React
 - Three.js
-
-
-
-
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 
@@ -8,9 +8,8 @@ import ReactGA from "react-ga4";
 // OLD 'UA-215876059-1'
 ReactGA.initialize("G-HC2JMRH067");
 
-ReactDOM.render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
+  </StrictMode>,
 );
